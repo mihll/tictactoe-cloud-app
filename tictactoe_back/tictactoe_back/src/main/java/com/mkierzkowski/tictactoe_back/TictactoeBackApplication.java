a551e0845f97b44ded209d3b2859397e7cdd5ec5
Michał Kierzkowski <kierzkowski89@gmail.com>
2022-10-31 22:26:33 +0100

    Added backend project

---
+++ tictactoe_back/tictactoe_back/src/main/java/com/mkierzkowski/tictactoe_back/TictactoeBackApplication.java
@@ -0,0 +1,13 @@
+package com.mkierzkowski.tictactoe_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TictactoeBackApplication.class, args);
+    }
+
+}
---
+++ tictactoe_back/tictactoe_back/src/test/java/com/mkierzkowski/tictactoe_back/TictactoeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkierzkowski.tictactoe_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

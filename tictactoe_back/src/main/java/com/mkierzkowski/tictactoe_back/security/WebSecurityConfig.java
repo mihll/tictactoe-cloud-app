b214ed91ca5c1eea67aa6e331ff6338c7c9b5587
Michał Kierzkowski <kierzkowski89@gmail.com>
2023-01-19 20:50:48 +0100

    Backend - fixed CORS and build on Docker

--- tictactoe_back/src/main/java/com/mkierzkowski/tictactoe_back/security/WebSecurityConfig.java
+++ tictactoe_back/src/main/java/com/mkierzkowski/tictactoe_back/security/WebSecurityConfig.java
@@ -4,10 +4,8 @@ import com.mkierzkowski.tictactoe_back.exception.CustomAuthenticationEntryPoint;
 import com.mkierzkowski.tictactoe_back.exception.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,12 +16,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] APP_AUTHENTICATION_WHITELIST = {
@@ -73,9 +68,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .cors()
-                .and()
+        http.cors().and().csrf().disable()
                 .formLogin()
                 .loginPage("/api/login")
                 .failureHandler(customAuthenticationFailureHandler)
@@ -106,10 +99,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedOriginPatterns(List.of(CorsConfiguration.ALL));
+        configuration.setAllowedMethods(List.of(CorsConfiguration.ALL));
+        configuration.setAllowedHeaders(List.of(CorsConfiguration.ALL));
         configuration.setAllowCredentials(true);
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
+
